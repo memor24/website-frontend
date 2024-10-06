09e15a02b18580f521b72e493d06e82f334d15d2
Mehdi <112832477+memor24@users.noreply.github.com>
2024-10-06 14:22:07 -0600

    Update fe_view_count_total.js

--- webpage/fe_view_count_total.js
+++ webpage/fe_view_count_total.js
@@ -1,10 +1,10 @@
 
-// function to fetch counter from ddb via lambda & updates totalviews
+//function to get counter from ddb via lambda & updates totalviews
 const dataContainer = document.getElementById('totalviews');
 
 async function getCounter() {
     try {
-        let response = await fetch("https://xw2wtu2vvpdidknzxcumdgcgri0miekq.lambda-url.us-west-2.on.aws/"); // Directly invokes lambda
+        let response = await fetch("https://xw2wtu2vvpdidknzxcumdgcgri0miekq.lambda-url.us-west-2.on.aws/"); //directly invokes lambda endpoint
         let data = await response.json();
         dataContainer.textContent = `${data}`; 
 
